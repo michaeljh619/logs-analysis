e1329fffcc13390ba9433fe935097c02e85f5c87
vagrant <vagrant@vagrant.vm>
2018-10-10 01:51:14 +0000

    feat: querying and file writing for answer 3 added

--- Main.py
+++ Main.py
@@ -30,6 +30,22 @@ Q2_3 = " where path != '/' and SUBSTR(path, 10) = slug"
 Q2_4 = " and authors.id = articles.author group by authors.name"
 Q2_5 = " order by num desc;"
 QUERY2 = Q2_1 + Q2_2 + Q2_3 + Q2_4 + Q2_5
+Q3_1 = "select subq1.date,"
+Q3_2 = " ((CAST(subq1.num AS float))"
+Q3_3 = "/(CAST(subq1.num + subq2.num AS float))) as percent from"
+Q3_4 = " (select time::timestamp::date as date,"
+Q3_5 = " count(*) as num from log"
+Q3_6 = " where status = '404 NOT FOUND'"
+Q3_7 = " group by date) as subq1,"
+Q3_8 = " (select time::timestamp::date as date,"
+Q3_9 = " count(*) as num from log"
+Q3_10 = " where status = '200 OK'"
+Q3_11 = " group by date) as subq2"
+Q3_12 = " where subq1.date = subq2.date"
+Q3_13 = " and ((CAST(subq1.num AS float))"
+Q3_14 = "/(CAST(subq1.num + subq2.num AS float))) >= 0.01"
+Q3_15 = " order by percent desc;"
+QUERY3 = Q3_1+Q3_2+Q3_3+Q3_4+Q3_5+Q3_6+Q3_7+Q3_8+Q3_9+Q3_10+Q3_11+Q3_12+Q3_13+Q3_14+Q3_15;
 
 # perform queries
 results1 = query(QUERY1)
@@ -40,8 +56,12 @@ results2 = query(QUERY2)
 results2_str = '\n'.join(str(x) for x in results2)
 full_answer2 = Q2_1+"\n"+Q2_2+"\n"+Q2_3+"\n"+Q2_4+"\n"+Q2_5+"\n\n"+ results2_str
 
+results3 = query(QUERY3)
+results3_str = '\n'.join(str(x) for x in results3)
+full_answer3 = QUERY3 + "\n\n" + results3_str
+
 # write the results to a file
 clear_results()
 write_results(1, full_answer1)
 write_results(2, full_answer2)
-#write_results(3, "Eating!")
+write_results(3, full_answer3)
