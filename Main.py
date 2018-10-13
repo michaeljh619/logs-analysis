317212c05c04ebc0f3c8646e7d2b0cccb03fbc14
vagrant <vagrant@vagrant.vm>
2018-10-13 13:50:46 +0000

    fix: output is now a neat, readable string

--- Main.py
+++ Main.py
@@ -15,41 +15,44 @@ from ReadQuery import read_query_file
 from QueryHandler import query
 from optparse import OptionParser
 
-# authorship
-__author__ = "Michael J. Hart"
-__email__ = "MichaelJHart760@gmail.com"
+# main code
+if __name__ == "__main__":
+    # authorship
+    __author__ = "Michael J. Hart"
+    __email__ = "MichaelJHart760@gmail.com"
 
-# command line options
-parser = OptionParser()
-parser.add_option("-f", "--file", action="store_true",
-                  help="write results to results.txt", default=False)
-(options, args) = parser.parse_args()
+    # command line options
+    parser = OptionParser()
+    parser.add_option("-f", "--file", action="store_true",
+                      help="write results to results.txt",
+                      default=False)
+    (options, args) = parser.parse_args()
 
-# query files
-QUERY_FILE_1 = "query1.txt"
-QUERY_FILE_2 = "query2.txt"
-QUERY_FILE_3 = "query3.txt"
+    # query files
+    QUERY_FILE_1 = "query1.txt"
+    QUERY_FILE_2 = "query2.txt"
+    QUERY_FILE_3 = "query3.txt"
 
-# get queries
-query_str_1 = read_query_file(QUERY_FILE_1)
-query_str_2 = read_query_file(QUERY_FILE_2)
-query_str_3 = read_query_file(QUERY_FILE_3)
+    # get queries
+    query_str_1 = read_query_file(QUERY_FILE_1)
+    query_str_2 = read_query_file(QUERY_FILE_2)
+    query_str_3 = read_query_file(QUERY_FILE_3)
 
-# perform queries
-results1 = query(query_str_1)
-results2 = query(query_str_2)
-results3 = query(query_str_3)
+    # perform queries
+    results1 = query(query_str_1)
+    results2 = query(query_str_2)
+    results3 = query(query_str_3)
 
-# turn query lists to strings
-results_str_1 = '\n'.join(str(x) for x in results1)
-results_str_2 = '\n'.join(str(x) for x in results2)
-results_str_3 = '\n'.join(str(x) for x in results3)
+    # turn query lists to strings
+    #results_str_1 = '\n'.join(str(x) for x in results1)
+    #results_str_2 = '\n'.join(str(x) for x in results2)
+    #results_str_3 = '\n'.join(str(x) for x in results3)
 
-# print the results, may be to a file depending on CL options
-if options.file:
-    clear_results()
-print_results(1, results_str_1, options.file)
-print_results(2, results_str_2, options.file)
-print_results(3, results_str_3, options.file)
-if options.file:
-    print("Results written to file: 'results.txt'")
+    # print the results, may be to a file depending on CL options
+    if options.file:
+        clear_results()
+    print_results(1, results1, options.file)
+    print_results(2, results2, options.file)
+    print_results(3, results3, options.file)
+    if options.file:
+        print("Results written to file: 'results.txt'")
--- PrintResults.py
+++ PrintResults.py
@@ -6,10 +6,24 @@ for this project. When write_results is called, it will automatically
 write out to a file called 'results.txt'.
 '''
 
+# imports
+import datetime
+
 # result headers
 RH1 = "1.) What are the three most popular articles?\n"
 RH2 = "2.) Who are the most popular authors?\n"
 RH3 = "3.) Which days did 1% or more of requests lead to errors?\n"
+
+# string formats
+q1_beg_str = "The article "
+q1_mid_str = " has a total of "
+q1_end_str = " views."
+q2_beg_str = "The author "
+q2_mid_str = " has a total of "
+q2_end_str = " views to their articles."
+q3_beg_str = "On "
+q3_mid_str = " there was a total of "
+q3_end_str = " percent of requests that led to errors."
 
 # file
 RESULTS_FILE_NAME = "results.txt"
@@ -35,7 +49,8 @@ def print_results(result_num, query_results, write_to_file=False):
     else:
         header = RH3
     # string to write
-    results_str = header + "\n" + query_results + "\n\n"
+    query_results_str = results_to_str(result_num, query_results)
+    results_str = header + "\n" + query_results_str + "\n"
     # if write to file
     if write_to_file:
         # open file, write header
@@ -45,3 +60,49 @@ def print_results(result_num, query_results, write_to_file=False):
         results_file.close()
     else:
         print(results_str)
+
+
+# formats the list of results into a neat string
+# result_num: numbered question you are answering
+# query_results: the results of the query in a string
+def results_to_str(result_num, query_results):
+    # strings for formatting
+    beg_str = ""
+    mid_str = ""
+    end_str = ""
+    final_str = ""
+    # query results list of strings
+    query_list_str = []
+
+    # build surrounding strings
+    if result_num == 1:
+        beg_str = q1_beg_str
+        mid_str = q1_mid_str
+        end_str = q1_end_str
+    elif result_num == 2:
+        beg_str = q2_beg_str
+        mid_str = q2_mid_str
+        end_str = q2_end_str
+    else:
+        beg_str = q3_beg_str
+        mid_str = q3_mid_str
+        end_str = q3_end_str
+
+    # turn queries into list of 2-tuple strings
+    if result_num == 1 or result_num == 2:
+        for (name, count) in query_results:
+            query_list_str.append(("'%s'" % name, str(count)))
+    else:
+        for (date, perc) in query_results:
+            query_list_str.append((date.strftime('%m/%d/%y'),
+                                   str(perc*100.0)))
+
+    # create readable queries
+    index = 1
+    for (r1, r2) in query_list_str:
+        final_str += str(index) + "- "
+        final_str += beg_str + r1 + mid_str + r2 + end_str
+        final_str += "\n"
+        index += 1
+    # return final string
+    return final_str
